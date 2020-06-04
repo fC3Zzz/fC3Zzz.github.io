8716f25345ada59bbcc6a035e24405a007d14fd4
fcez <lavrnenko20077@gmail.com>
2020-06-04 11:39:44 +0700

    change name file9 commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+
+
+function registration(){
+    let input_data = prompt("pochta","")
+    if (input_data.includes("@gmail.com")){
+        // alert("horosho")
+        // document.getElementsByClassName("about")[0].style.background="red";
+        alert("ok")
+    }
+    else{
+        // alert("ploho")
+        // document.getElementsByClassName("about")[0].style.background="white";
+        alert("try again")
+    
+    }
+    
+    let input_data2 = prompt("login","")
+    if (input_data.includes("@")){
+        // alert("horosho")
+        // document.getElementsByClassName("about")[0].style.background="red";
+        alert("try again")
+    }
+    else{
+        // alert("ploho")
+        // document.getElementsByClassName("about")[0].style.background="white";
+        alert(ok)
+    }
+}
+
+function accept(value){
+ 
+}
